3020ff2f063fdfa570f1dfe742e55bb1e0aa813d
Martin del Rosario <delrosariomartin23@gmail.com>
2022-09-05 21:43:07 +0200

    functions updated

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 function enviarFormulario() {
     if(document.formPlanes.nombre.value || document.formPlanes.apellido.value || document.formPlanes.dni.value || document.formPlanes.email.value || document.formPlanes.telefono.value == ""){
         document.getElementById("camposObligatorios").innerHTML = "Todos los campos son obligatorios"
+    }else{
+        document.getElementById("mensajeBienvenida").innerHTML = "Bienvenido " + document.formPlanes.nombre.value
     }
 }
 
