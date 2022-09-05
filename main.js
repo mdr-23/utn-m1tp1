4e4bae6608bbba1b00302d903d4b14f7a8b87b56
Martin del Rosario <delrosariomartin23@gmail.com>
2022-09-05 23:38:14 +0200

    select and functions updated

--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 function enviarFormulario() {
-    if(document.formPlanes.nombre.value || document.formPlanes.apellido.value || document.formPlanes.dni.value || document.formPlanes.email.value || document.formPlanes.telefono.value == ""){
+    if(document.formPlanes.nombre.value && document.formPlanes.apellido.value && document.formPlanes.dni.value && document.formPlanes.email.value && document.formPlanes.telefono.value == ""){
         document.getElementById("camposObligatorios").innerHTML = "Todos los campos son obligatorios"
     }else{
-        document.getElementById("mensajeBienvenida").innerHTML = "Bienvenido " + document.formPlanes.nombre.value
+        document.getElementById("mensajeBienvenida").innerHTML = "¡Gracias por elegirnos, " + document.formPlanes.nombre.value + '!'
     }
 }
 
 function mostrarPrecio(){
 
-    let select = document.getElementById("planes").value;
+    let tipoSeguro = document.getElementById("planes").value;
 
-    let mensaje = document.getElementById("planes");
-    let seleccionado = mensaje.options[mensaje.selectedIndex].text;
-    alert(seleccionado + ": " + select)
+    let precio;
+
+    switch(tipoSeguro){
+        case "1": precio = 500; break;
+        case "2": precio = 1000; break;
+        case "3": precio = 1500; break;
+    }
+
+    console.log(precio);
+
+    document.getElementById("planElegido").innerHTML = "El valor de tu plan es de " + "$" + precio
 
 }
 
